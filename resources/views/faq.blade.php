7b702eca5361ecf234f405a5f2f787e8a3a64a71
wes <weshenshall@gmail.com>
2019-04-30 23:50:57 -0400

    removed unnecessary faq question and replaced with actual question

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -10,10 +10,10 @@
       <div class="left">
         <ul>
           <li><a href="#faq-0">How long did you spend on the coding test? What would you add to your solution if you had more time? If you didn't spend much time on the coding test then use this as an opportunity to explain what you would add.</a></li>
-          <li><a href="#faq-1">Can anyone use Laravel Lightening?</a></li>
-          <li><a href="#faq-2"> What was the most useful feature that was added to the latest version of your chosen language? Please include a snippet of code that shows how you've used it.  </a></li>
+          <li><a href="#faq-1"> What was the most useful feature that was added to the latest version of your chosen language? Please include a snippet of code that shows how you've used it.  </a></li>
+          <li><a href="#faq-2"> How would you track down a performance issue in production? Have you ever had to do this?  </a></li>
           <li><a href="#faq-3"> How would you improve the API that you just used? </a></li>
-          <li><a href="#faq-4"> Please describe yourself using JSON. </a></li>
+          <li><a href="#faq-4"> Please describe yourself using JSON.  </a></li>
         </ul>
       </div>
       <div class="right">
